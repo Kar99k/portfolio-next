63a7c7e0688b75a8c00afa69afa4886ff0f7fb1a
kar99k <krthkeyanlee@gmail.com>
2024-11-28 21:27:38 +0530

    transistions

--- components/organism/MDXRenderer.tsx
+++ components/organism/MDXRenderer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 import { ArrowLeft } from "lucide-react";
 import { MDXRemote } from "next-mdx-remote";
 import { useRouter } from "next/navigation";
@@ -12,24 +13,30 @@ const MDXRenderer = ({ serializedContent }: { serializedContent: any }) => {
   };
 
   return (
-    <article className="container prose mx-auto py-8 px-6 xl:py-16 xl:px-12 flex flex-col gap-12">
-      <div>
-        <button
-          className="flex gap-3 text-foreground-500 hover:text-orange-9 no-underline"
-          onClick={handleGoBack}
-        >
-          <ArrowLeft width={16} />
-          Back
-        </button>
-      </div>
-      {serializedContent && (
+    <motion.div
+      animate={{ opacity: 1 }} // Target state, opacity will animate to 1
+      initial={{ opacity: 0 }} // Initial state, opacity is 0
+      transition={{ duration: 1 }} // Duration of the animation (in seconds)
+    >
+      <article className="container prose mx-auto py-8 px-6 xl:py-16 xl:px-12 flex flex-col gap-12">
         <div>
-          <MDXRemote {...serializedContent} />
+          <button
+            className="flex gap-3 text-foreground-500 hover:text-orange-9 no-underline"
+            onClick={handleGoBack}
+          >
+            <ArrowLeft width={16} />
+            Back
+          </button>
         </div>
-      )}
+        {serializedContent && (
+          <div>
+            <MDXRemote {...serializedContent} />
+          </div>
+        )}
 
-      {!serializedContent && <div>No Post Found</div>}
-    </article>
+        {!serializedContent && <div>No Post Found</div>}
+      </article>
+    </motion.div>
   );
 };
 
