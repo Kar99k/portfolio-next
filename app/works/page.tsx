41ec80c35e39b577667a3719d311192e671793b0
kar99k <krthkeyanlee@gmail.com>
2024-12-11 17:45:33 +0530

    - package updated

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -2,7 +2,7 @@ import Works from "@/components/organism/Works";
 
 const page = () => {
   return (
-    <section className="container mx-auto max-w-screen-xl xl:py-8">
+    <section className="container mx-auto xl:py-8">
       <Works buttonVisible={false} />
     </section>
   );
