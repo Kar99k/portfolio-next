c7db5340a7eafdcda436a640ebe535eea06cffcb
kar99k <krthkeyanlee@gmail.com>
2024-12-21 01:22:48 +0530

    - testimonial

--- components/molecules/TestimonialCard.tsx
+++ components/molecules/TestimonialCard.tsx
@@ -1,10 +1,21 @@
 import NameTag from "../atom/NameTag";
 
-const TestimonialCard = ({ name, designation, comment }: TestimonialCard) => {
+const TestimonialCard = ({
+  name,
+  designation,
+  comment,
+  photoUrl,
+  linkedin,
+}: TestimonialCard) => {
   return (
-    <div className="flex flex-col gap-4 p-8 border-1 rounded-lg border-gray-4">
-      <div className="font-maison-light text-base xl:text-lg italic">{`" ${comment} "`}</div>
-      <NameTag designation={designation} name={name} />
+    <div className="flex flex-col gap-4 p-8 border-1 rounded-lg border-gray-4 h-fit w-[420px]">
+      <div className="font-maison-light text-base xl:text-lg italic h-fit">{`" ${comment} "`}</div>
+      <NameTag
+        designation={designation}
+        linkedin={linkedin}
+        name={name}
+        photoUrl={photoUrl}
+      />
     </div>
   );
 };
--- components/organism/Testimonials.tsx
+++ components/organism/Testimonials.tsx
@@ -1,22 +1,30 @@
 import SectionTemplate from "../templates/SectionTemplate";
 import TestimonialCard from "../molecules/TestimonialCard";
+import Marquee from "../ui/marquee";
 
 import { testimonials } from "@/data/data";
 const Testimonials = () => {
   return (
     <SectionTemplate>
       <div>Testimonials</div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-        {testimonials.map((item, index) => {
-          return (
-            <TestimonialCard
-              key={index}
-              comment={item.comment}
-              designation={item.designation}
-              name={item.name}
-            />
-          );
-        })}
+      <div className="relative w-full overflow-hidden">
+        <div className="absolute left-0 top-0 bottom-0 w-16 pointer-events-none z-10 bg-gradient-to-r from-BG to-transparent" />
+        <div className="absolute right-0 top-0 bottom-0 w-16 pointer-events-none z-10 bg-gradient-to-l from-BG to-transparent" />
+
+        <Marquee pauseOnHover className="[--duration:20s] w-full" >
+          {testimonials.map((item, index) => {
+            return (
+              <TestimonialCard
+                key={index}
+                comment={item.comment}
+                designation={item.designation}
+                linkedin={item.linkedin}
+                name={item.name}
+                photoUrl={item.photoUrl}
+              />
+            );
+          })}
+        </Marquee>
       </div>
     </SectionTemplate>
   );
