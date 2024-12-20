f10b8f68239861ee030b965c9ef9c5a5605fc739
kar99k <krthkeyanlee@gmail.com>
2024-12-20 23:26:43 +0530

    - open to work implement

--- components/organism/IntroContent.tsx
+++ components/organism/IntroContent.tsx
@@ -1,21 +1,14 @@
 import Image from "next/image";
-import { Button } from "@nextui-org/button";
 
 import LinkButtons from "../molecules/LinkButton";
+import SpecialButton from "../atom/SpecialButton";
 
 const IntroContent = () => {
   return (
     <div className="flex flex-col gap-4 p-4 md:p-8 xl:p-12 xl:gap-8">
       <div className="flex justify-between">
         <Image alt="avatars" height={103} src="/image/avatar.png" width={106} />
-        <Button
-          color="success"
-          radius="sm"
-          startContent={<div>👩🏽‍💻</div>}
-          variant="flat"
-        >
-          <p>Open to Work</p>
-        </Button>
+          <SpecialButton />
       </div>
       <span className="text-3xl xl:text-4xl max-w-screen-lg">
         Hey, I’m Karthikeyan—a <i>passionate</i> Frontend Engineer and tech
