95eb82d030cbf8b5c592aa3a350f18b756dcc8d7
kar99k <krthkeyanlee@gmail.com>
2024-12-23 15:25:39 +0530

    refactor: about page

---
+++ app/about/layout.tsx
@@ -0,0 +1,9 @@
+const layout = ({ children }) => {
+  return (
+    <article className="container mx-auto max-w-screen-lg flex flex-col">
+      {children}
+    </article>
+  );
+};
+
+export default layout;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,18 @@
-export default async function About() {
-  return <main>About</main>;
+import NotionPage from "@/components/organism/NotionPage";
+import { fetchRecordMap } from "@/services/fetchData";
+
+export default async function Page() {
+  let recordMap: any;
+
+  try {
+    recordMap = await fetchRecordMap("163848fc79c680a5aa07d1b1b916bba9");
+  } catch {
+    return (
+      <main>
+        <p>No Page Found</p>
+      </main>
+    );
+  }
+
+  return <NotionPage recordMap={recordMap} />;
 }
